399d16e0185839661dfde9fe5ac126b7c9ad23f2
Georgi Stavrev <stavrev.georgi@gmail.com>
2017-07-25 15:24:42 +0300

    initial

---
+++ test/sample.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('A Feature', function () {
+    describe('A Scenario', function () {
+        it('A test case', function () {
+            assert.ok(true);
+        });
+    });
+});
